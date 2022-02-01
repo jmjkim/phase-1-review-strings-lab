937518d2c58e22381d4397cb76a15c44f8842dc1
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-01-31 23:58:37 -0800

    commit tree 773a2ab7e661098317efff036be42f7d3cc9b8fc

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = "john wick";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, ${currentUser.toUpperCase()}!`;
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
